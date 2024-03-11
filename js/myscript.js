82ee1c2fc9a83acbab33148f1b3b4c79553862a3
TaherCCG <taher.ccg@gmail.com>
2024-03-11 14:35:46 +0000

    my first jquery script

---
+++ js/myscript.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('h2').css('text-decoration', 'underline');
+    $('ul').css('border', '1px solid #ccc');
+
+    $('h2').removeClass('underline');
+    $('ul').addClass('border');
+
+});
